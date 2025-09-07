1e7842b88b77320f19dd05095b6beb75f42462ef
filagate03 <alexfarley555@gmail.com>
2025-09-08 02:09:15 +0300

    add frontend/lib/models/category.dart

---
+++ frontend/lib/models/category.dart
@@ -0,0 +1,20 @@
+class Category {
+  final int id;
+  final String name;
+  final String? description;
+
+  Category({
+    required this.id,
+    required this.name,
+    this.description,
+  });
+
+  // A factory constructor for creating a new Category instance from a map.
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+    );
+  }
+}
